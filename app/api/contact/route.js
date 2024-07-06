794b75644eff24b211686b17145600dbbb6424f8
karthik <karthiknadar1204@gmail.com>
2024-07-06 19:55:37 +0530

    resend config almost done

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -1,85 +1,72 @@
-// import { NextResponse } from "next/server";
-// import nodemailer from "nodemailer";
+// import { Resend } from 'resend';
+// import KoalaWelcomeEmail from "@/emails/index";
+// import { NextResponse } from 'next/server';
 
-// const user = process.env.EMAIL;
-// const pass = process.env.PASSWORD;
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
 // export async function POST(request) {
 //   try {
-//     const { name, email,subject, message } = await request.json();
-
-//     const transporter = nodemailer.createTransport({
-//       service: "zoho",
-//       host: "smtp.zoho.in",
-//       port: 465,
-//       secure: true,
-//       auth: {
-//         user,
-//         pass,
-//       },
+//     const { name, email,subject ,message } = request.body;
+//     const { data, error } = await resend.emails.send({
+//       from: 'karthiknadar1204@gmail.com',
+//       to: email,
+//       subject: subject,
+//       react: KoalaWelcomeEmail({ 
+//         name,
+//         email,
+//         subject,
+//         message
+//        }), // Provide necessary props if required
 //     });
 
-//     const mailOptions = {
-//       from: user,
-//       to: "karthiknadar1204@gmail.com",
-//       subject: "New message from your-website",
-//       text: `Name: ${name}\nEmail: ${email}\n${subject}\nMessage: ${message}`,
-//     };
+//     if (error) {
+//       return NextResponse.json(error, { status: 400 });
+//     }
 
-//     await transporter.sendMail(mailOptions);
-
-//     return NextResponse.json(
-//       { message: "Message sent successfully" },
-//       { status: 200 },
-//     );
+//     return NextResponse.json({
+//       status: 'ok',
+//       data,
+//     });
 //   } catch (error) {
-//     console.error(error);
-//     return new NextResponse("Failed to send message.", { status: 500 });
+//     return NextResponse.json({ error: error.message }, { status: 500 });
 //   }
 // }
 
 
 
 
+import { Resend } from 'resend';
+import KoalaWelcomeEmail from "@/emails/index";
+import { NextResponse } from 'next/server';
 
-
-
-import { NextResponse } from "next/server";
-import nodemailer from "nodemailer";
-
-const user = process.env.EMAIL;
-const pass = process.env.PASSWORD;
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(request) {
   try {
-    const { name, email, subject, message } = await request.json();
+    const body = await request.json();
+    const { name, email, subject, message } = body;
 
-    const transporter = nodemailer.createTransport({
-      service: "zoho",
-      host: "smtp.zoho.in",
-      port: 465,
-      secure: true,
-      auth: {
-        user,
-        pass,
-      },
+    const { data, error } = await resend.emails.send({
+      from: 'onboarding@resend.dev',
+      to: email,
+      subject: subject,
+      react: KoalaWelcomeEmail({ 
+        name,
+        email,
+        subject,
+        message
+      }), // Provide necessary props if required
     });
 
-    const mailOptions = {
-      from: user,
-      to: "karthiknadar1204@gmail.com",
-      subject: `New message from your-website: ${subject}`,
-      text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
-    };
+    if (error) {
+      return NextResponse.json(error, { status: 400 });
+    }
 
-    await transporter.sendMail(mailOptions);
-
-    return NextResponse.json(
-      { message: "Message sent successfully" },
-      { status: 200 }
-    );
+    return NextResponse.json({
+      status: 'ok',
+      data,
+    });
   } catch (error) {
-    console.error(error);
-    return new NextResponse("Failed to send message.", { status: 500 });
+    return NextResponse.json({ error: error.message }, { status: 500 });
   }
 }
