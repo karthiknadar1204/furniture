2ff8d4d7fabba70388c668c08fc5e67374f08827
karthik <karthiknadar1204@gmail.com>
2024-07-12 09:53:30 +0530

    fixed responsiveness

--- app/category/[categoryname]/[subcategoryname]/[categoryId]/[subcategoryid]/page.jsx
+++ app/category/[categoryname]/[subcategoryname]/[categoryId]/[subcategoryid]/page.jsx
@@ -50,7 +50,7 @@ const CategoryPage = () => {
   const [loading, setLoading] = useState(false);
   const [categoryName, setCategoryName] = useState("");
   const [subcategoryName, setSubcategoryName] = useState("");
-  const [priceRange, setPriceRange] = useState([0, 100000]);
+  const [priceRange, setPriceRange] = useState([0, 1000]);
 
   const fetchProducts = async () => {
     try {
@@ -59,8 +59,7 @@ const CategoryPage = () => {
       const categoryName = parts[2].toLowerCase();
       const subcategoryName = parts[3].toLowerCase();
       const category = predefinedCategories.find(
-        (cat) =>
-          cat.name.toLowerCase().replace(/\s+/g, "") === categoryName
+        (cat) => cat.name.toLowerCase().replace(/\s+/g, "") === categoryName
       );
       const categoryId = category ? category.id : null;
       const subcategoryId = parseInt(parts[5], 10);
@@ -116,8 +115,8 @@ const CategoryPage = () => {
       {loading ? (
         <Loader className="animate-spin text-gray-600 h-12 w-12" />
       ) : (
-        <div className="flex w-full">
-          <div className="w-1/4 p-4">
+        <div className="flex flex-col md:flex-row w-full">
+          <div className="md:w-1/4 p-4">
             <h2 className="text-lg font-semibold mb-4">Filter by Price</h2>
             <div className="flex items-center mb-4">
               <span className="mr-2">$0</span>
@@ -128,7 +127,7 @@ const CategoryPage = () => {
                 step="10"
                 value={priceRange[0]}
                 onChange={(e) => handlePriceChange(e, 0)}
-                className="flex-grow"
+                className="flex-grow appearance-none bg-black h-1 rounded-md"
               />
               <span className="ml-2">$100000</span>
             </div>
@@ -141,7 +140,7 @@ const CategoryPage = () => {
                 step="10"
                 value={priceRange[1]}
                 onChange={(e) => handlePriceChange(e, 1)}
-                className="flex-grow"
+                className="flex-grow appearance-none bg-black h-1 rounded-md"
               />
               <span className="ml-2">$100000</span>
             </div>
@@ -150,13 +149,13 @@ const CategoryPage = () => {
             </p>
             <button
               onClick={handleFilterClick}
-              className="bg-blue-500 text-white px-4 py-2 rounded"
+              className="bg-black text-white px-4 py-2 rounded"
             >
               Apply Filter
             </button>
           </div>
-          <div className="w-3/4">
-            <div className="w-full text-left mb-4 mt-8 lg:ml-[38rem] sm:ml-[8rem]">
+          <div className="md:w-3/4 flex flex-col">
+            <div className="w-full text-left mb-4 md:ml-8">
               <h2 className="text-gray-500 text-sm">
                 {categoryName} / {subcategoryName}
               </h2>
@@ -167,7 +166,7 @@ const CategoryPage = () => {
                 No products added yet.
               </p>
             ) : (
-              <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-8 ml-8">
+              <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-8 md:ml-8">
                 {productsList.map((product) => (
                   <div
                     key={product.id}
@@ -190,7 +189,9 @@ const CategoryPage = () => {
                     <h2 className="text-lg font-semibold mb-2 text-center">
                       {product.name}
                     </h2>
-                    <p className="text-gray-700 text-center">${product.price}</p>
+                    <p className="text-gray-700 text-center">
+                      ${product.price}
+                    </p>
                   </div>
                 ))}
               </div>
