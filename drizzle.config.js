5c1902d2b91241711225a4e764cd65f4f3ad640d
karthik <karthiknadar1204@gmail.com>
2024-07-13 22:09:25 +0530

    change environment variable

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,6 +3,6 @@ export default {
     schema: "./configs/schema.js",
     dialect: 'postgresql',
     dbCredentials: {
-      url: 'postgresql://karthiknadar1204:Fvph9DyfVm2L@ep-restless-credit-a1c7489o.ap-southeast-1.aws.neon.tech/Furniture_3.0?sslmode=requiree',
+      url: process.env.NEXT_PUBLIC_DRIZZLE_DATABASE_URL,
     }
   };
--- utils/UserUtils.js
+++ utils/UserUtils.js
@@ -26,23 +26,3 @@ export function  checkUserRole(session) {
   return null;
 }
 
-
-
-// utils/UserUtils.js
-
-// export const checkUserRole = (session) => {
-//   // Ensure session and roles are available
-//   if (!session || !session.publicUserData || !session.publicUserData.roles) {
-//     return 'guest'; // Assuming guest role for no session or roles
-//   }
-
-//   // Check if the user has the 'org:admin' role
-//   if (session.publicUserData.roles.includes('org:admin')) {
-//     return 'admin';
-//   }
-
-//   // Add more role checks as needed
-
-//   // Default to 'user' role if no specific role matches
-//   return 'user';
-// };
