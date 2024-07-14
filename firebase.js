ccd16696683e20e279aff0bd6183f783ba5f4f95
karthik <karthiknadar1204@gmail.com>
2024-07-14 09:25:49 +0530

    added env variables for firebase

--- firebase.js
+++ firebase.js
@@ -8,13 +8,13 @@ import {getStorage} from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCJDXp8833Xo4In-GeAgB581H1MJOb3IXs",
-  authDomain: "furniture-54935.firebaseapp.com",
-  projectId: "furniture-54935",
-  storageBucket: "furniture-54935.appspot.com",
-  messagingSenderId: "354851814658",
-  appId: "1:354851814658:web:d58f983c979f68c2f2561b",
-  measurementId: "G-B2KWBJ9DXG"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSENGER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
