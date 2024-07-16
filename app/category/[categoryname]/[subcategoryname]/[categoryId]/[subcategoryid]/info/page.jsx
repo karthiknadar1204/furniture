c761ca278a53f68c5c3c22b8eefebcd1028a7f72
karthik <karthiknadar1204@gmail.com>
2024-07-16 21:23:06 +0530

    font fix on info page

--- app/category/[categoryname]/[subcategoryname]/[categoryId]/[subcategoryid]/info/page.jsx
+++ app/category/[categoryname]/[subcategoryname]/[categoryId]/[subcategoryid]/info/page.jsx
@@ -1,14 +1,14 @@
-"use client";
+"use client"
 
-import { useRouter, usePathname } from "next/navigation";
-import { db } from '@/configs';
-import { products } from '@/configs/schema';
-import { eq } from 'drizzle-orm';
 import { useEffect, useState, useRef } from 'react';
 import Image from 'next/image';
 import { Loader } from 'lucide-react';
 import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { useRouter, usePathname } from "next/navigation";
+import { db } from '@/configs';
+import { products } from '@/configs/schema';
+import { eq } from 'drizzle-orm';
 
 const ProductInfo = () => {
   const router = useRouter();
@@ -103,18 +103,20 @@ const ProductInfo = () => {
               Out of Stock
             </span>
           )}
-          <hr className="w-60 border-t border-gray-400 opacity-50 mt-4"/>
+          <hr className="w-full sm:w-240 border-t border-gray-400 opacity-50 mt-4"/>
           <p className="text-2xl text-red-600 font-bold mt-8">${product.price}</p>
-          <hr className="w-60 border-t border-gray-400 opacity-50 mt-4"/>
-          <p className="text-sm mt-4">SKU: {product.id}</p>
-          <p className="text-sm mt-2">Category: {product.category}</p>
-          <p className="text-sm mt-2">Subcategory: {product.subcategory}</p>
-          <hr className="w-60 border-t border-gray-400 opacity-50 mt-4"/>
+          <hr className="w-full sm:w-240 border-t border-gray-400 opacity-50 mt-4"/>
+          <p className="text-sm mt-4">SKU: <span className="font-bold">{product.id}</span></p>
+          <p className="text-sm mt-2">Category: <span className="font-bold">{product.category}</span></p>
+          <p className="text-sm mt-2">Subcategory: <span className="font-bold">{product.subcategory}</span></p>
+          <hr className="w-full sm:w-240 border-t border-gray-400 opacity-50 mt-4"/>
           {/* Displaying dimensions */}
           <p className="text-sm mt-2">Dimensions:</p>
-          <p className="text-sm mt-2">Length: {product.length} cm</p>
-          <p className="text-sm mt-2">Width: {product.breadth} cm</p>
-          <p className="text-sm mt-2">Height: {product.height} cm</p>
+          <div className="flex flex-wrap">
+            <p className="text-sm mt-2 mr-2">Length:<span className="font-bold">{product.length} cm</span></p>
+            <p className="text-sm mt-2 mr-2">Width: <span className="font-bold">{product.breadth} cm</span></p>
+            <p className="text-sm mt-2 mr-2">Height:<span className="font-bold">{product.height} cm</span></p>
+          </div>
         </div>
       </div>
       <div className="w-full sm:w-3/4 lg:w-2/3 mt-36"> 
