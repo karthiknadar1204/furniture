a13c4a8d79c69cff1089d36ee98b7766008ad656
karthik <karthiknadar1204@gmail.com>
2024-07-21 17:18:40 +0530

    added images in product range

--- app/_components/ProductRange.jsx
+++ app/_components/ProductRange.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BedDouble } from 'lucide-react';
+import { Sofa } from 'lucide-react';
+import Image from 'next/image';
 
 const ProductRange = () => {
   return (
@@ -9,53 +11,53 @@ const ProductRange = () => {
       </div>
       <div className="grid grid-cols-1 md:grid-cols-5 gap-20">
         <div className="flex flex-col items-center">
-          <BedDouble size={64} />
+        <Image src='/BedSideTable.png' width={94} height={94} />
           <h1 className="text-sm mt-2"> <strong>Bed Side Table</strong></h1>
           <h1 className="text-sm">21 items</h1>
         </div>
         <div className="flex flex-col items-center">
-          <BedDouble size={64} />
-          <h1 className="text-sm mt-2"><strong>Bed Side Table</strong></h1>
+          <Image src='/WardRobe.png' width={94} height={94} />
+          <h1 className="text-sm mt-2"><strong>Wardrobe</strong></h1>
           <h1 className="text-sm">21 items</h1>
         </div>
         <div className="flex flex-col items-center">
           <BedDouble size={64} />
-          <h1 className="text-sm mt-2"><strong>Bed Side Table</strong></h1>
+          <h1 className="text-sm mt-2"><strong>Dresser</strong></h1>
+          <h1 className="text-sm">21 items</h1>
+        </div>
+        <div className="flex flex-col items-center">
+          <Sofa size={64} />
+          <h1 className="text-sm mt-2"><strong>Sofa</strong></h1>
+          <h1 className="text-sm">21 items</h1>
+        </div>
+        <div className="flex flex-col items-center">
+        <Image src='/CenterTable.png' width={144} height={124} />
+          <h1 className="text-sm mt-2"><strong>Center Table</strong></h1>
+          <h1 className="text-sm">21 items</h1>
+        </div>
+        <div className="flex flex-col items-center">
+        <Image src='/SofaCumBed.png' width={94} height={94} />
+          <h1 className="text-sm mt-2"><strong>Sofa-cum bed</strong></h1>
+          <h1 className="text-sm">21 items</h1>
+        </div>
+        <div className="flex flex-col items-center">
+        <Image src='/DiningTable.png' width={94} height={94} />
+          <h1 className="text-sm mt-2"><strong>Dining Table</strong></h1>
+          <h1 className="text-sm">21 items</h1>
+        </div>
+        <div className="flex flex-col items-center">
+        <Image src='/OfficeTable.png' width={94} height={94} />
+          <h1 className="text-sm mt-2"><strong>OfficeTable</strong></h1>
           <h1 className="text-sm">21 items</h1>
         </div>
         <div className="flex flex-col items-center">
           <BedDouble size={64} />
-          <h1 className="text-sm mt-2"><strong>Bed Side Table</strong></h1>
+          <h1 className="text-sm mt-2"><strong>Office Chair</strong></h1>
           <h1 className="text-sm">21 items</h1>
         </div>
         <div className="flex flex-col items-center">
           <BedDouble size={64} />
-          <h1 className="text-sm mt-2"><strong>Bed Side Table</strong></h1>
-          <h1 className="text-sm">21 items</h1>
-        </div>
-        <div className="flex flex-col items-center">
-          <BedDouble size={64} />
-          <h1 className="text-sm mt-2"><strong>Bed Side Table</strong></h1>
-          <h1 className="text-sm">21 items</h1>
-        </div>
-        <div className="flex flex-col items-center">
-          <BedDouble size={64} />
-          <h1 className="text-sm mt-2"><strong>Bed Side Table</strong></h1>
-          <h1 className="text-sm">21 items</h1>
-        </div>
-        <div className="flex flex-col items-center">
-          <BedDouble size={64} />
-          <h1 className="text-sm mt-2"><strong>Bed Side Table</strong></h1>
-          <h1 className="text-sm">21 items</h1>
-        </div>
-        <div className="flex flex-col items-center">
-          <BedDouble size={64} />
-          <h1 className="text-sm mt-2"><strong>Bed Side Table</strong></h1>
-          <h1 className="text-sm">21 items</h1>
-        </div>
-        <div className="flex flex-col items-center">
-          <BedDouble size={64} />
-          <h1 className="text-sm mt-2"><strong>Bed Side Table</strong></h1>
+          <h1 className="text-sm mt-2"><strong>Mattress</strong></h1>
           <h1 className="text-sm">21 items</h1>
         </div>
       </div>
