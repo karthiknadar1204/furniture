8dd14d0b05ec3748e0741a62abbcdd102b05f7b6
karthik <karthiknadar1204@gmail.com>
2024-08-16 12:34:19 +0530

    images updated

--- app/_components/Slider.jsx
+++ app/_components/Slider.jsx
@@ -10,17 +10,20 @@ const Slider = dynamic(() => import("react-slick"), { ssr: false });
 
 const sliderData = [
   {
-    image: "https://images.unsplash.com/photo-1604014237800-1c9102c219da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
+    // image: "https://images.unsplash.com/photo-1604014237800-1c9102c219da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
+    image:"/1.jpg",
     title: "Let us find your Forever Home.",
     description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt illo tenetur fuga ducimus numquam ea!",
   },
   {
-    image: "https://images.unsplash.com/photo-1604014237800-1c9102c219da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
+    // image: "https://images.unsplash.com/photo-1604014237800-1c9102c219da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
+    image:"/2.jpg",
     title: "Find your perfect spot.",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vehicula justo at urna.",
   },
   {
-    image: "https://images.unsplash.com/photo-1604014237800-1c9102c219da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
+    // image: "https://images.unsplash.com/photo-1604014237800-1c9102c219da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
+    image:"/3.jpg",
     title: "Discover new places.",
     description: "Fusce dapibus tellus vel metus venenatis, ut posuere nulla efficitur. Sed vel metus vel metus.",
   },
@@ -46,7 +49,7 @@ const Carousel = () => {
               className="h-full w-full bg-cover bg-center bg-no-repeat"
               style={{ backgroundImage: `url(${slide.image})` }}
             >
-              <div className="absolute inset-0 bg-white/75 sm:bg-transparent sm:from-white/95 sm:to-white/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l"></div>
+              <div className="absolute inset-0 sm:bg-transparent sm:from-white/95 sm:to-white/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l"></div>
               <div className="relative mx-auto h-full w-full max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8">
                 <div className="max-w-xl text-center ltr:sm:text-left rtl:sm:text-right">
                   <motion.h1
