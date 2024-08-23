3af1bbef39d70fe660d2170c3ff475ac5ff88b16
karthik <karthiknadar1204@gmail.com>
2024-08-23 13:30:33 +0530

    update-3

--- app/_components/Slider.jsx
+++ app/_components/Slider.jsx
@@ -83,11 +83,10 @@
 
 
 
-
-"use client"
+"use client";
 
 import React from "react";
-import dynamic from 'next/dynamic';
+import dynamic from "next/dynamic";
 import { motion } from "framer-motion";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -96,19 +95,22 @@ const Slider = dynamic(() => import("react-slick"), { ssr: false });
 
 const sliderData = [
   {
-    image:"/1.jpg",
+    image: "/1.jpg",
     title: "Let us find your Forever Home.",
-    description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt illo tenetur fuga ducimus numquam ea!",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt illo tenetur fuga ducimus numquam ea!",
   },
   {
-    image:"/2.jpg",
+    image: "/2.jpg",
     title: "Find your perfect spot.",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vehicula justo at urna.",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin vehicula justo at urna.",
   },
   {
-    image:"/3.jpg",
+    image: "/3.jpg",
     title: "Discover new places.",
-    description: "Fusce dapibus tellus vel metus venenatis, ut posuere nulla efficitur. Sed vel metus vel metus.",
+    description:
+      "Fusce dapibus tellus vel metus venenatis, ut posuere nulla efficitur. Sed vel metus vel metus.",
   },
 ];
 
@@ -127,7 +129,10 @@ const Carousel = () => {
     <section className="relative h-screen w-screen -mt-24">
       <Slider {...settings}>
         {sliderData.map((slide, index) => (
-          <div key={index} className="relative h-screen w-screen overflow-x-hidden">
+          <div
+            key={index}
+            className="relative h-screen w-screen overflow-x-hidden"
+          >
             <div
               className="h-full w-full bg-cover bg-center bg-no-repeat"
               style={{ backgroundImage: `url(${slide.image})` }}
@@ -139,10 +144,17 @@ const Carousel = () => {
                     initial={{ opacity: 0, y: 50 }}
                     animate={{ opacity: 1, y: 0 }}
                     transition={{ duration: 0.5 }}
-                    className="text-3xl font-extrabold sm:text-5xl text-white"
+                    className="text-3xl font-extrabold sm:text-5xl text-white lg:text-black"
                   >
                     {slide.title.split(" ").map((word, i) => (
-                      <span key={i} className={i === 3 ? "block font-extrabold text-rose-700" : ""}>
+                      <span
+                        key={i}
+                        className={
+                          i === 3
+                            ? "block font-extrabold text-rose-700 lg:text-black"
+                            : ""
+                        }
+                      >
                         {word}{" "}
                       </span>
                     ))}
@@ -151,7 +163,7 @@ const Carousel = () => {
                     initial={{ opacity: 0, y: 50 }}
                     animate={{ opacity: 1, y: 0 }}
                     transition={{ duration: 0.7 }}
-                    className="mt-4 sm:mt-6 max-w-lg sm:text-xl/relaxed text-white"
+                    className="mt-4 sm:mt-6 max-w-lg sm:text-xl/relaxed text-white lg:text-black"
                   >
                     {slide.description}
                   </motion.p>
