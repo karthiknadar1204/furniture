61dbbb9f0d721277adaae861db9ab6ce9d55d802
karthik <karthiknadar1204@gmail.com>
2024-08-28 16:51:59 +0530

    video fix

--- app/_components/ExtraImages.jsx
+++ app/_components/ExtraImages.jsx
@@ -1,11 +1,52 @@
+// import React from 'react';
+
+// const ExtraImages = () => {
+//   return (
+//     <div className="flex flex-col p-2 sm:p-4 overflow-x-hidden">
+//       <div className="w-full mb-4 sm:mb-6 relative">
+//         <iframe
+//           className="w-[calc(100%+20px)] sm:w-full h-48 sm:h-72 mx-auto block"
+//           src="https://www.youtube.com/embed/7Bbev7V6lj4"
+//           title="YouTube video player"
+//           frameBorder="0"
+//           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+//           allowFullScreen
+//         ></iframe>
+//       </div>
+//       <div className="w-full mb-4 sm:mb-6 relative">
+//         <iframe
+//           className="w-[calc(100%+20px)] sm:w-full h-48 sm:h-72 mx-auto block"
+//           src="https://www.youtube.com/embed/mjPtSgfk-hY"
+//           title="YouTube video player"
+//           frameBorder="0"
+//           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+//           allowFullScreen
+//         ></iframe>
+//       </div>
+//       <div className="w-full relative">
+//         <iframe
+//           className="w-[calc(100%+20px)] sm:w-full h-48 sm:h-72 mx-auto block"
+//           src="https://www.youtube.com/embed/7Bbev7V6lj4"
+//           title="YouTube video player"
+//           frameBorder="0"
+//           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+//           allowFullScreen
+//         ></iframe>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default ExtraImages;
+
 import React from 'react';
 
 const ExtraImages = () => {
   return (
     <div className="flex flex-col p-2 sm:p-4 overflow-x-hidden">
-      <div className="w-full mb-4 sm:mb-6 relative">
+      <div className="w-full mb-4 sm:mb-6 relative w-custom-container">
         <iframe
-          className="w-[calc(100%+20px)] sm:w-full h-48 sm:h-72 mx-auto block"
+          className="w-[calc(100%+50px)] sm:w-full h-48 sm:h-72 mx-auto block w-custom"
           src="https://www.youtube.com/embed/7Bbev7V6lj4"
           title="YouTube video player"
           frameBorder="0"
@@ -13,9 +54,9 @@ const ExtraImages = () => {
           allowFullScreen
         ></iframe>
       </div>
-      <div className="w-full mb-4 sm:mb-6 relative">
+      <div className="w-full mb-4 sm:mb-6 relative w-custom-container">
         <iframe
-          className="w-[calc(100%+20px)] sm:w-full h-48 sm:h-72 mx-auto block"
+          className="w-[calc(100%+50px)] sm:w-full h-48 sm:h-72 mx-auto block w-custom"
           src="https://www.youtube.com/embed/mjPtSgfk-hY"
           title="YouTube video player"
           frameBorder="0"
@@ -23,9 +64,9 @@ const ExtraImages = () => {
           allowFullScreen
         ></iframe>
       </div>
-      <div className="w-full relative">
+      <div className="w-full relative w-custom-container">
         <iframe
-          className="w-[calc(100%+20px)] sm:w-full h-48 sm:h-72 mx-auto block"
+          className="w-[calc(100%+50px)] sm:w-full h-48 sm:h-72 mx-auto block w-custom"
           src="https://www.youtube.com/embed/7Bbev7V6lj4"
           title="YouTube video player"
           frameBorder="0"
